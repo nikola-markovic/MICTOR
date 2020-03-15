bde79fbb8e7cf991967708b915b053778f055e44
Nikola Marković <nikolamarkovic1991@yahoo.com>
2020-03-15 23:12:12 +0100

    Init Xcode project
    - Create Xcode project with SwiftUI and Unit Tests

---
+++ MICTOR/MICTOR/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MICTOR
+//
+//  Created by Nikola on 15/03/2020.
+//  Copyright © 2020 Win Games. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
